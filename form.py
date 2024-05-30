8986a1790802a39eb685805d6dd18367b6ed0528
Gladwel <chelangatgladwel9@gmail.com>
2024-05-30 10:44:55 +0300

    added a blog

---
+++ form.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import DataRequired,Length,Email,EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username =StringField('Username', validators=[DataRequired(), Length(min=4,max=12)])
+    email =StringField('Email' , validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[Length(min=4,max=12), DataRequired() ])
+    confirm_password =PasswordField('confirm password',validators=[DataRequired(),EqualTo('password')])
+    submit =SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email =StringField('Email' , validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember =BooleanField('Remember me')
+    submit =SubmitField('Login')
+ 
